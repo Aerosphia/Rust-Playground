568b8bca37119af7a92d836a84c9d03db874204d
Justin K <justinkolaras@outlook.com>
2022-03-09 22:33:55 -0800

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-// Non-functional program
-
-use std::ops::AddAssign<&&i32>;
-
 fn add_numbers(integers: &[&i32]) -> i32 {
     let mut net: i32 = 0;
     for i in integers {
-        net += i;
+        net += **i;
     }
     return net;
 }
 
 fn main() {
-    println!("{}", add_numbers(5, 10));
+    println!("{}", add_numbers(&[&5, &5, &10])); // -> 20
 }
