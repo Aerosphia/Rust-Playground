82b0a3bab09fd6cbd789d7d1923223fb68752018
Justin K <justin.kolaras0@gmail.com>
2022-04-20 04:31:41 -0700

    Small refactor

--- files/even_nums_recursive.rs
+++ files/even_nums_recursive.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let evens: Vec<i32> = even_nums(vec![2, 1, 4, 3, 7, 6], 0);
+    let evens: Vec<i32> = even_nums(vec![2, 1, 4, 3, 7, 6]);
     println!("{:?}", evens);
 }
 
-fn even_nums(mut vector: Vec<i32>, index: usize) -> Vec<i32> {
+pub fn even_nums(vector: Vec<i32>) -> Vec<i32> {
+    return even_nums_rec(vector, 0);
+}
+
+fn even_nums_rec(mut vector: Vec<i32>, index: usize) -> Vec<i32> {
     if index >= vector.len() {
         return vector;
     }
@@ -14,5 +18,5 @@ fn even_nums(mut vector: Vec<i32>, index: usize) -> Vec<i32> {
         vector.remove(index);
     }
 
-    return even_nums(vector, if is_even { index + 1 } else { index });
+    return even_nums_rec(vector, if is_even { index + 1 } else { index });
 }
