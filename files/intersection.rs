80d1786aa29d64c5a047f5584bde4188b4f9dab2
Justin K <justin.kolaras0@gmail.com>
2022-04-30 23:55:18 -0700

    Indentation

--- files/intersection.rs
+++ files/intersection.rs
@@ -4,23 +4,23 @@
 use std::collections::HashMap;
 
 fn main() {
-  let inter = intersection(&[1, 2, 3, 4, 5], &[2, 4, 6]);
-  println!("{:?}", inter);
+    let inter = intersection(&[1, 2, 3, 4, 5], &[2, 4, 6]);
+    println!("{:?}", inter);
 }
 
 fn intersection(arr: &[i32], arr2: &[i32]) -> Vec<i32> {
-  let mut first_arr_vals: HashMap<i32, bool> = HashMap::new();
-  let mut identical_vals: Vec<i32> = Vec::new();
+    let mut first_arr_vals: HashMap<i32, bool> = HashMap::new();
+    let mut identical_vals: Vec<i32> = Vec::new();
 
-  for i in arr.iter() {
-    first_arr_vals.insert(*i, true);
-  }
-
-  for i in arr2.iter() {
-    if first_arr_vals.contains_key(i) {
-      identical_vals.push(*i);
+    for i in arr.iter() {
+        first_arr_vals.insert(*i, true);
     }
-  }
 
-  identical_vals
+    for i in arr2.iter() {
+        if first_arr_vals.contains_key(i) {
+            identical_vals.push(*i);
+        }
+    }
+
+    identical_vals
 }
