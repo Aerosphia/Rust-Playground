b050ae9b9350b807e0d79a2d48ca83d468bccac8
Justin K <justin.kolaras0@gmail.com>
2022-05-29 09:10:44 -0700

    Refactor

--- files/minigrep/src/utils.rs
+++ files/minigrep/src/utils.rs
@@ -1,6 +1,9 @@
 use std::process;
 
-pub fn run_and_exit<F: Fn()>(func: F, code: i32) -> ! {
+pub fn run_and_exit<F>(func: F, code: i32) -> !
+where
+    F: Fn(),
+{
     func();
     process::exit(code);
 }
